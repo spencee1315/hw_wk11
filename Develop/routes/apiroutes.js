3a42274fc305168b120e093f87b984e21d51d517
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-09-04 15:06:13 -0700

    updated to include api delete route

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -47,4 +47,32 @@ module.exports = function(app) {
         res.send('Thank you for your note!');
     });
 
-}
+    // API DELETE Requests
+    // API route that allows user to delete a note and updates json data
+    app.delete('/api/notes/:id', function(req, res) {
+        // Gets id number of note to delete
+        const deleteNote = req.params.id;
+        console.log(deleteNote);
+
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+
+            // Comparing each note's id to delete note
+            dbData = JSON.parse(data);
+            // for each function, comparing each note's id to the chose_for_death variable
+            for (let i = 0; i < dbData.length; i++) {
+                if (dbData[i].id === Number(deleteNote)) {
+                    dbData.splice([i], 1);
+                }
+            }
+            console.log(dbData);
+            stringData = JSON.stringify(dbData);
+
+            fs.writeFile('./db/db.json', stringData, (err, data) => {
+                if (err) throw err;
+            });
+        });
+        // Express resonse.status(204)
+        res.status(204).send();
+    });
+};
